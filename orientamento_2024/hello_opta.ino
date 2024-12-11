7069335af624386b9e30e0c38ff83f3d4c872f87
ingdifilippo <davidedifilippo@iisdaostalaquila.it>
2024-12-11 18:28:07 +0100

    Create hello_opta.ino
    codice per orientamento

---
+++ orientamento_2024/hello_opta.ino
@@ -0,0 +1,38 @@
+// Define an array to hold the pin numbers for Opta's user LEDs.
+const int USER_LEDS[] = {LED_D0, LED_D1, LED_D2, LED_D3};
+
+// Number of Opta's user LEDs
+const int NUM_LEDS = 4;
+
+void setup() {
+  // Set the mode for each user LED to OUTPUT.
+  for (int i = 0; i < NUM_LEDS; i++) {
+    pinMode(USER_LEDS[i], OUTPUT);
+  }
+}
+
+void loop() {
+  // Scan from the first LED to the last.
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // Turn on the LED.
+    // Wait for 50 milliseconds.
+    digitalWrite(USER_LEDS[i], HIGH); 
+    delay(50); 
+    // Turn off the LED.
+    // Wait for 50 milliseconds.
+    digitalWrite(USER_LEDS[i], LOW); 
+    delay(50); 
+  }
+  
+  // Scan back from the last LED to the first.
+  for (int i = NUM_LEDS - 1; i >= 0; i--) {
+    // Turn on the LED.
+    // Wait for 50 milliseconds.
+    digitalWrite(USER_LEDS[i], HIGH); 
+    delay(50); 
+    // Turn off the LED.
+    // Wait for 50 milliseconds.
+    digitalWrite(USER_LEDS[i], LOW); 
+    delay(50); 
+  }
+}
