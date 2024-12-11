8a552a5d33af8fb1bd8fa39ee5ee7d4d8a8059bf
ingdifilippo <davidedifilippo@iisdaostalaquila.it>
2024-12-11 19:30:40 +0100

    Update hello_opta.ino
    Aggiunto il controllo con interrupt della pressione di un pulsante

--- orientamento_2024/hello_opta.ino
+++ orientamento_2024/hello_opta.ino
@@ -1,41 +1,117 @@
-//Viene effettuata un'attivazione dei relè in sequenza diretta e inversa
 
+// Current and previous state of the button.
+volatile bool buttonPressed = false;
 
-// Define an array to hold the pin numbers for Opta's user LEDs.
-const int USER_LEDS[] = {LED_D0, LED_D1, LED_D2, LED_D3};
+// Counter to keep track of the LED sequence.
+int counter = 0;
 
-// Number of Opta's user LEDs
-const int NUM_LEDS = 4;
+// Variables to implement button debouncing.
+unsigned long lastDebounceTime  = 0;
+unsigned long debounceDelay     = 150;
+
+unsigned long waitDelay = 5000;
+unsigned long lastCounterTime = 0;
+
+// Array to store LED pins.
+int LEDS[] = {LED_D0, LED_D1, LED_D2, LED_D3};
+int NUM_LEDS = 4;
+
+// Arrays of relays and user LEDs
+int relayOutputs[] = {D0, D1, D2, D3};
+// Compute the number of relays/LEDs based on the size of the relayPins array
+int numRelays = 4;
+
 
 void setup() {
-  // Set the mode for each user LED to OUTPUT.
-  for (int i = 0; i < NUM_LEDS; i++) {
-    pinMode(USER_LEDS[i], OUTPUT);
+  // Initialize Opta user LEDs.
+  for(int i = 0; i < NUM_LEDS; i++) {
+    pinMode(LEDS[i], OUTPUT);
+    pinMode(relayOutputs[i], OUTPUT); 
   }
+  
+  // Set up the interrupt on USER_BTN to trigger on a rising edge (when the button is pressed)
+  attachInterrupt(digitalPinToInterrupt(A0), buttonISR, FALLING);
+}
+
+/**
+  Interrupt service routine (ISR)
+  Set the variable buttonPressed to true
+
+  @param None.
+*/
+void buttonISR() {
+  buttonPressed = true;
 }
 
 void loop() {
-  // Scan from the first LED to the last.
+  // If the button was pressed
+  if (buttonPressed) {
+    // Debouncing routine.
+     lastCounterTime = millis();
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+      lastDebounceTime = millis();
+
+      // Only increment the counter if the new button state is HIGH.
+      if(counter < NUM_LEDS){
+        counter++;
+      }
+      else{
+        counter = 0;
+      }
+      
+      // Reset the button pressed flag
+      buttonPressed = false;
+    }
+  }
+
+   if ((millis() - lastCounterTime) > waitDelay) {
+      
+
+      // Scan from the first LED to the last.
   for (int i = 0; i < NUM_LEDS; i++) {
     // Turn on the LED.
     // Wait for 50 milliseconds.
-    digitalWrite(USER_LEDS[i], HIGH); 
-    delay(50); 
-    // Turn off the LED.
-    // Wait for 50 milliseconds.
-    digitalWrite(USER_LEDS[i], LOW); 
-    delay(50); 
+    digitalWrite(LEDS[i], HIGH); 
+    digitalWrite(relayOutputs[i], HIGH);
+    delay(200); 
+    
   }
   
   // Scan back from the last LED to the first.
   for (int i = NUM_LEDS - 1; i >= 0; i--) {
     // Turn on the LED.
     // Wait for 50 milliseconds.
-    digitalWrite(USER_LEDS[i], HIGH); 
-    delay(50); 
-    // Turn off the LED.
-    // Wait for 50 milliseconds.
-    digitalWrite(USER_LEDS[i], LOW); 
-    delay(50); 
+    
+    digitalWrite(LEDS[i], LOW); 
+    digitalWrite(relayOutputs[i], LOW); 
+    delay(200); 
+  }
+  
+  }
+
+
+  // Control LED states.
+  changeLights();
+}
+
+/**
+  Control the status LEDs based on a counter value.
+  Turns off all LEDs first, then turns on the LED 
+  corresponding to the current counter value.
+
+  @param None.
+*/
+void changeLights() {
+  // Turn off all user LEDs.
+  for(int i = 0; i < NUM_LEDS; i++) {
+    digitalWrite(LEDS[i], LOW);
+    digitalWrite(relayOutputs[i], LOW);
+  }
+
+  // Turn on the selected user LED.
+  if(counter > 0) {
+    digitalWrite(relayOutputs[counter-1], HIGH);
+    digitalWrite(LEDS[counter - 1], HIGH);
+    
   }
 }
